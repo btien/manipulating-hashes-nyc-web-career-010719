61dbd804fd029b566e5fa1b243d8dd354b593f5f
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-06 19:46:30 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        value.delete_if(|flavor| flavor=="strawberry")
+        value.delete_if{|flavor| flavor=="strawberry"}
          #if :favorite_icecream_flavors.include?{"strawberry"}
          # :favorite_ice_cream_flavors.delete("strawberry")
            
